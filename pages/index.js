fd72e6a548f09c780cba9ed18cd617e8651ac1a1
Cody Durham <codydd@gmail.com>
2021-07-18 23:57:33 -0600

    Add createClient / and .env file to house API key and token

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,16 @@
+import { createClient } from 'contentful'
+
+export async function getStaticProps() {
+  const client = createClient( {
+    // contentful_space_id is in the .env file. 'Process' is a built in method for next.js
+    space: process.env.CONTENTFUL_SPACE_ID, 
+    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN, // content delivery API - access token (settings -> api)
+  })
+
+}
+
+
+
 export default function Recipes() {
   return (
     <div className="recipe-list">
