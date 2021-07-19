c55d0616511c5896c869e587e742ecfc1fa47d5b
Cody Durham <codydd@gmail.com>
2021-07-19 00:31:47 -0600

    Add env file with variables. Restart server/ working now

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,30 @@
 import { createClient } from 'contentful'
+const contentful = require('contentful')
+
+
 
 export async function getStaticProps() {
-  const client = createClient( {
-    // contentful_space_id is in the .env file. 'Process' is a built in method for next.js
-    space: process.env.CONTENTFUL_SPACE_ID, 
+ 
+ 
+  const client = createClient ({
+    space: process.env.CONTENTFUL_SPACE_ID, // contentful_space_id is in the .env file. 'Process' is a built in method for next.js
     accessToken: process.env.CONTENTFUL_ACCESS_TOKEN, // content delivery API - access token (settings -> api)
   })
+  
+  const res = await client.getEntries({ content_type: 'recipe'})
+  
+  return {
+    props : {
+      recipes: res.items // these are all the items we get back from the response from 'recipe'
+    }
+  }
 
 }
 
 
 
-export default function Recipes() {
+export default function Recipes({ recipes }) {
+  console.log('recipes from site', recipes);
   return (
     <div className="recipe-list">
       Recipe List
